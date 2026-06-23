92d7ed354459a25970189a5a9e0ae55cc87a378d
Louis Theys <louis.theys@hainaut-ea.be>
2026-06-23 23:07:41 +0200

    modification opérateur de propagation de nullité

--- GolfTaskManager/ChefEquipe.cs
+++ GolfTaskManager/ChefEquipe.cs
@@ -35,7 +35,7 @@ public class ChefEquipe : Ouvrier
 
         foreach (var t in tachesAttribuees)
         {
-            string ouvrier = t.AssigneA.Prenom;
+            string ouvrier = t.AssigneA?.Prenom ?? "Non attribué";
             string zone = t.Zone != null ? t.Zone.Nom : "Non attribuée";
             string heure = string.IsNullOrWhiteSpace(t.HeurePrevue) ? "Non définie" : t.HeurePrevue;
 
